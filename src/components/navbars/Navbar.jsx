7a102c03a77aa7e1be319a7fc164f3cef575c9a6
oskcarsv <oskcarqsv@gmail.com>
2024-05-22 09:47:28 -0600

    Update Navbar component styles and logic
    This commit updates the styles and logic of the Navbar component. The changes include:
    - Formatting the code with Prettier and StandardJS
    - Fixing style issues introduced in previous commits
    - Updating class names and CSS styles
    - Refactoring the handleLogout function
    - Improving the conditional rendering of buttons based on user authentication and roles
    
    Details: [GitHub PR #13](https://github.com/oskcarsv/front-hotel-management/pull/13)

--- src/components/navbars/Navbar.jsx
+++ src/components/navbars/Navbar.jsx
@@ -1,72 +1,67 @@
-import { jwtDecode } from 'jwt-decode'
-import { useState } from 'react'
-import '../styles/Navbar.css'
-import '../styles/Variables.css'
-import toast from 'react-hot-toast'
+import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
+import "../styles/Navbar.css";
+import "../styles/Variables.css";
+import toast from "react-hot-toast";
 
 export const Navbar = ({ redirectHome }) => {
-  let saved = localStorage.getItem('user')
+  let saved = localStorage.getItem("user");
 
-  saved = JSON.parse(saved)
+  saved = JSON.parse(saved);
 
   const handleLogout = () => {
-    localStorage.removeItem('user')
-    saved = null
-    window.location.href = './'
-  }
+    localStorage.removeItem("user");
+    saved = null;
+    window.location.href = "./";
+  };
 
-  const isLoginPage = window.location.pathname === '/auth'
+  const isLoginPage = window.location.pathname === "/auth";
 
-  const allowedRoles = ['SUPER_ROLE', 'ADMIN_BOSS_ROLE', 'ADMIN_EMPLOYEE_ROLE']
+  const allowedRoles = ["SUPER_ROLE", "ADMIN_BOSS_ROLE", "ADMIN_EMPLOYEE_ROLE"];
 
   return (
-    <nav className='navbar'>
-      <div className='tittle-container'>
-        <h1 className='text-title-main'>YouHotel</h1>
+    <nav className="navbar">
+      <div className="tittle-container">
+        <h1 className="text-title-main">YouHotel</h1>
       </div>
 
-      <div className='buttons-container'>
-        {redirectHome
-          ? (
-            <a className='button' href='./'>
-              Home
+      <div className="buttons-container">
+        {redirectHome ? (
+          <a className="button" href="./">
+            Home
+          </a>
+        ) : saved == null ? (
+          <a className="button" href="/auth">
+            Login
+          </a>
+        ) : allowedRoles.includes(saved.user.role) ? (
+          <div className="lineal">
+            <a className="button" href="./">
+              Add Hotel
             </a>
-            )
-          : saved == null
-            ? (
-              <a className='button' href='/auth'>
-                Login
+            <a className="button" href="./">
+              User
+            </a>
+            <a className="button" href="#" onClick={handleLogout}>
+              Exit
+            </a>
+          </div>
+        ) : (
+          <div className="buttons">
+            {/* <div className="lineal">
+              <a className="button" href="./" onClick={handleLogout}>
+                User
               </a>
-              )
-            : allowedRoles.includes(saved.user.role)
-              ? (
-                <div className='lineal'>
-                  <a className='button' href='./'>
-                    Add Hotel
-                  </a>
-                  <a className='button' href='./'>
-                    User
-                  </a>
-                  <a className='button' href='#' onClick={handleLogout}>
-                    Exit
-                  </a>
-                </div>
-                )
-              : (
-                <a className='button' href='#' onClick={handleLogout}>
-                  <>
-                    <div className='lineal'>
-                      <a className='button' href='./' onClick={handleLogout}>
-                        User
-                      </a>
-                      <a className='button' href='#' onClick={handleLogout}>
-                        Exit
-                      </a>
-                    </div>
-                  </>
-                </a>
-                )}
+            </div> */}
+
+            <div className="lineal">
+              <a className="button" href="#" onClick={handleLogout}>
+                Exit
+              </a>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
-  )
-}
+  );
+};
