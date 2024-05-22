ef9ef68553065ff4fb1cd1632b9e9d0775aeaa01
oskcarsv <oskcarqsv@gmail.com>
2024-05-22 08:55:26 -0600

    Update Register component styles
    This commit updates the styles of the Register component to improve the layout and alignment. The changes include:
    - Updating the flexbox properties for better alignment
    - Adjusting the padding and box-sizing
    - Modifying the font family, size, and color
    
    Details: [GitHub PR #11](https://github.com/oskcarsv/front-hotel-management/pull/11)

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
-import { Input } from './Input'
+import { useState } from "react";
+import { Input } from "./Input";
 import {
   emailValidationMessage,
   validatePasswordMessage,
@@ -11,180 +11,164 @@ import {
   validateConfirPassword,
   validateEmail,
   validatePassword,
-  validateNameMessage
-} from '../shared/validators'
-import { useRegister } from '../shared/hooks/useRegister'
-import './Register.css'
+  validateNameMessage,
+} from "../shared/validators";
+import { useRegister } from "../shared/hooks/useRegister";
+import "./Register.css";
 
 export const Register = ({ switchAuthHandler, onRegister }) => {
-  const { register, isLoading } = useRegister()
+  const { register, isLoading } = useRegister();
 
   const [formState, setFormState] = useState({
     name: {
-      value: '',
+      value: "",
       isValid: false,
-      showError: false
+      showError: false,
     },
     email: {
-      value: '',
+      value: "",
       isValid: false,
-      showError: false
+      showError: false,
     },
     password: {
-      value: '',
+      value: "",
       isValid: false,
-      showError: false
+      showError: false,
     },
     passwordConfirm: {
-      value: '',
+      value: "",
       isValid: false,
-      showError: false
+      showError: false,
     },
     username: {
-      value: '',
+      value: "",
       isValid: false,
-      showError: false
-    }
-  })
+      showError: false,
+    },
+  });
 
   const handleInputValueChange = (value, field) => {
     setFormState((prevState) => ({
       ...prevState,
       [field]: {
         ...prevState[field],
-        value
-      }
-    }))
-  }
+        value,
+      },
+    }));
+  };
 
   const handleInputValidationOnBlur = (value, field) => {
-    let isValid = false
+    let isValid = false;
     switch (field) {
-      case 'name':
-        isValid = validateName(value)
-        break
-      case 'email':
-        isValid = validateEmail(value)
-        break
-      case 'password':
-        isValid = validatePassword(value)
-        break
-      case 'passwordConfirm':
-        isValid = validateConfirPassword(formState.password.value, value)
-        break
-      case 'username':
-        isValid = validateUsername(value)
-        break
+      case "name":
+        isValid = validateName(value);
+        break;
+      case "email":
+        isValid = validateEmail(value);
+        break;
+      case "password":
+        isValid = validatePassword(value);
+        break;
+      case "passwordConfirm":
+        isValid = validateConfirPassword(formState.password.value, value);
+        break;
+      case "username":
+        isValid = validateUsername(value);
+        break;
       default:
-        break
+        break;
     }
     setFormState((prevState) => ({
       ...prevState,
       [field]: {
         ...prevState[field],
         isValid,
-        showError: !isValid
-      }
-    }))
-  }
+        showError: !isValid,
+      },
+    }));
+  };
 
   const handleRegister = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     register(
       formState.name.value,
       formState.email.value,
       formState.password.value,
       formState.username.value
-    )
-    onRegister()
-  }
+    );
+    onRegister();
+  };
 
   const isSubmitButtonDisabled =
     isLoading ||
     !formState.password.isValid ||
     !formState.email.isValid ||
     !formState.passwordConfirm.isValid ||
-    !formState.username.isValid
+    !formState.username.isValid;
 
   return (
-    <div className='register-container'>
-      /*{' '}
-      <header>
-        <a
-          href='link_a_tu_imagen.jpg'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            src='link_a_tu_imagen.jpg'
-            alt='Imagen del hotel'
-            className='hotel-image'
-          />
-        </a>
-        <img src='link_a_tu_logo.jpg' alt='Logo del Hotel' className='logo' />
-      </header>{' '}
-      */
-      <main>
-        <h1>Register of Hotel</h1>
-        <form className='auth-form' onSubmit={handleRegister}>
+    <div className="frame-1">
+      <div className="contact-form">
+        <div className="login-1">Register</div>
+        <form className="auth-form" onSubmit={handleRegister}>
           <Input
-            field='name'
-            label='Nombre'
+            field="name"
+            label="Nombre"
             value={formState.name.value}
             onChangeHandler={handleInputValueChange}
-            type='text'
+            type="text"
             onBlurHandler={handleInputValidationOnBlur}
             showErrorMessage={formState.name.showError}
             validationMessage={validateNameMessage}
           />
           <Input
-            field='email'
-            label='Correo electrónico'
+            field="email"
+            label="Correo electrónico"
             value={formState.email.value}
             onChangeHandler={handleInputValueChange}
-            type='text'
+            type="text"
             onBlurHandler={handleInputValidationOnBlur}
             showErrorMessage={formState.email.showError}
             validationMessage={emailValidationMessage}
           />
           <Input
-            field='username'
-            label='Nombre de usuario'
+            field="username"
+            label="Nombre de usuario"
             value={formState.username.value}
             onChangeHandler={handleInputValueChange}
-            type='text'
+            type="text"
             onBlurHandler={handleInputValidationOnBlur}
             showErrorMessage={formState.username.showError}
             validationMessage={validateUsernameMessage}
           />
           <Input
-            field='password'
-            label='Contraseña'
+            field="password"
+            label="Contraseña"
             value={formState.password.value}
             onChangeHandler={handleInputValueChange}
-            type='password'
+            type="password"
             onBlurHandler={handleInputValidationOnBlur}
             showErrorMessage={formState.password.showError}
             validationMessage={validatePasswordMessage}
           />
           <Input
-            field='passwordConfirm'
-            label='Confirmar Contraseña'
+            field="passwordConfirm"
+            label="Confirmar Contraseña"
             value={formState.passwordConfirm.value}
             onChangeHandler={handleInputValueChange}
-            type='password'
+            type="password"
             onBlurHandler={handleInputValidationOnBlur}
             showErrorMessage={formState.passwordConfirm.showError}
             validationMessage={passwordConfirmationMessage}
           />
-          <button type='submit' disabled={isSubmitButtonDisabled}>
+          <button type="submit" disabled={isSubmitButtonDisabled}>
             Register
           </button>
         </form>
-        <span onClick={switchAuthHandler} className='auth-form-switch-label'>
+        <span onClick={switchAuthHandler} className="auth-form-switch-label">
           ¿Do you already have an account? Sign in here!
         </span>
-      </main>
+      </div>
     </div>
-  )
-}
+  );
+};
