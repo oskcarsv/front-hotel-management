3ed144c80573e723cc7eb65b890fe1a633a10856
plopez2019280 <plopez-2019280@kinal.edu.gt>
2024-05-21 23:29:00 -0600

    uploading styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import {
 } from 'react-router-dom'
 import { MainPage } from './pages/main/mainPage'
 import { AuthPage } from './pages/auth/AuthPage'
+import { EventPage } from './pages/event/eventPage'
 
 function AppRoutes () {
   const routes = useRoutes([
     { path: '/', element: <MainPage /> },
-    { path: '/auth', element: <AuthPage /> }
+    { path: '/auth', element: <AuthPage /> },
+    { path: '/event', element: <EventPage/>}
   ])
   return routes
 }
--- src/pages/event/eventPage.jsx
+++ src/pages/event/eventPage.jsx
@@ -1,10 +1,83 @@
-import { Navbar } from '../../components/navbars/Navbar'
-import { Footer } from '../../components/footer/Footer'
-import { useEvents } from '../../shared/hooks'
 
+import { FaInstagram } from "react-icons/fa";
+//import { Input } from '../../components/Input'//
 import './eventPage.css'
 
 export const EventPage = () => {
+<<<<<<< HEAD
+  return (
+    <div className="events-1">
+      <div className="navigation">
+        <div className="frame-1321316143">
+          <span className="you-hotel">
+            YouHotel
+          </span>
+        </div>
+        <div className="items">
+          <button className="button">
+              Events
+            </button>
+          <button className="button-1">
+            Reservations
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        <div className="container-3">
+          <div className="headline">
+            <div className="events-2">
+              Events
+            </div>
+            <span className="save-your-event-here">
+              save your event here
+            </span>
+          </div>
+          <div className="form">
+            <div className="container-1">
+              <div className="input">
+                <div className="first-name">
+                  First name
+                </div>
+                <input className="field"/>
+              </div>
+              <div className="input-1">
+                <div className="last-name">
+                  Last name
+                </div>
+                <input className="field-1"/>
+              </div>
+            </div>
+            <div className="input-2">
+              <div className="email-address">
+                Email address
+              </div>
+              <input className="field-2"/>
+            </div>
+            <div className="input-3">
+              <div className="what-event-are-you-planning-to-do">
+                What event are you planning to do?<br />
+              </div>
+              <input className="field-3"/>
+            </div>
+            <button className="button-2">
+            Submit
+            </button>
+          </div>
+        </div>
+        <div className="image-1">
+        </div>
+      </div>
+      <div className="navigation-footer">
+        <div className="items-1">
+        </div>
+        <div className="container-2">
+          <div className="divider">
+          </div>
+          <div className="what-event-are-you-planning-to-hold-today">
+            What event are you planning to hold today?<br />
+          </div>
+        </div>
+=======
   const {
     events,
     loading,
@@ -84,8 +157,74 @@ export const EventPage = () => {
         <button className='btn add-btn' onClick={handleAddEvent}>
           Add Event
         </button>
+>>>>>>> ac04fffe3a54395fb81b502f0dd1e5012e8d7838
       </div>
-      <Footer />
     </div>
   )
 }
+
+
+
+/*
+export const EventPage = () => {
+  return (
+    <div className='frame-1'>
+      <div className='contact-form'>
+        <div className='items'>
+
+          <div className='abut-us'>About Us</div>
+        </div>
+        <div className='container'>
+          <div className='container-1'>
+            <div className='headline'>
+              <div className='login-1'>Login</div>
+              <span className='welcome-back'>
+                Welcome back
+                <br />
+              </span>
+            </div>
+            <form className='form'>
+              <div className='input'>
+                <div className='user-or-email'>User or Email</div>
+                <div className='field'>
+                  <Input
+                    field='usernameOrEmail'
+                    type='text'
+                  />
+                </div>
+              </div>
+              <div className='input-1'>
+                <div className='password'>Password</div>
+                <div className='field-1'>
+                  <Input
+                    field='password'
+
+                    type='password'
+                  />
+                </div>
+              </div>
+              <div className='button'>
+                <button type='submit'>
+                  <span className='Lbutton'>Login</span>
+                </button>
+              </div>
+            </form>
+            <div className='input-2'>
+              <div className='have-you-not-registered-do-it-here'>
+                Have you not registered? Do it here
+                <br />
+              </div>
+              <div className='button-1'>
+                <span className='Lbutton'>Register</span>
+              </div>
+            </div>
+          </div>
+          <div className='image-1'>
+            <img alt='My Image' />
+          </div>
+        </div>
+        <div className='navigation-footer' />
+      </div>
+    </div>
+  )
+}*/
